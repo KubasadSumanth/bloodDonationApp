fdb10523de850d3b67e335de819a925a7ec18db0
KubasadSumanth <86420140+KubasadSumanth@users.noreply.github.com>
2022-04-30 10:49:49 +0530

    Initialize
    Initialize

---
+++ server/routes/login_routes.js
@@ -0,0 +1,24 @@
+const express =require('express')
+
+const logcntrl= require('../controller/Login_control')
+
+const router=express.Router()
+
+router.post('/sign', function(req, res){
+    logcntrl.SignUp
+  });
+  router.post('/login', function(req, res){
+    logcntrl.LoginID
+  });
+/*
+router.post("/userId",logcntrl.userId)
+router.post("/sign",logcntrl.SignUp)
+router.post("/login",logcntrl.LoginID)
+router.post("/rooms",logcntrl.createRooms)
+router.post("/BRooms",logcntrl.createBRooms)
+router.post("/joinRoom",logcntrl.joinRoom)
+router.post("/getrooms",logcntrl.getRooms)
+router.post("/deleterooms",logcntrl.deleteRoom)
+*/
+module.exports=router
+
